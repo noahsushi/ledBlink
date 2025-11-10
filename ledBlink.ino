bab79e0562cfe8e775952460240de756ab08e3b3
Noah Hersh <105130529+noahsushi@users.noreply.github.com>
2025-11-10 15:01:05 -0500

    led2 Blink

--- ledBlink.ino
+++ ledBlink.ino
@@ -10,7 +10,10 @@ void setup() {
 
 void loop() {
   digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
   delay(1000);
   digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
+  delay(1000);
 
 }
