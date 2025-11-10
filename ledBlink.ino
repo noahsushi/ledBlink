0597f3920aac7f22ae65c62454bca74ef14ec4c5
Noah Hersh <105130529+noahsushi@users.noreply.github.com>
2025-11-10 15:02:00 -0500

    added button

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,19 +1,20 @@
 int ledPin = 13;
-int ledPin2 = 14
-
+int ledPin2 = 14;
+int buttonPin = 15;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
-
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  digitalWrite(ledPin2, LOW);
-  delay(1000);
-
+  if(digitalRead(buttonPin == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
+    delay(1000);
+  }
 }
