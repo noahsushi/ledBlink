ba5c7dc71cb63ae8ce4593de034e4ddbc125f365
Noah Hersh <105130529+noahsushi@users.noreply.github.com>
2025-11-10 14:57:44 -0500

    added led

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,9 +1,11 @@
+int ledPin = 13;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
